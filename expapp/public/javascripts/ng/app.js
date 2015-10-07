50023a971b78d03a181b36a813a40d18f29d5179
mdylag <miroslawq.dylag@gmail.com>
2015-10-07 13:55:31 +0200

    Directive.

--- expapp/public/javascripts/ng/app.js
+++ expapp/public/javascripts/ng/app.js
@@ -20,22 +20,36 @@ app.config(function($routeProvider) {
 
 app.controller('MainCtrl', function($scope, AppVersion, PeopleService) {
     $scope.version = AppVersion;
+    $scope.value = "Directive";
     console.log(PeopleService);
 });
 
 app.controller('PersonCtrl', function($scope, PeopleService) {
     $scope.person = {};
     $scope.save = function(form) {
-
+        if (form.$valid) {
+            PeopleService.save($scope.person);
+        }
     };
 });
 
 
-app.controller('PeopleCtrl', function($scope, $http) {
+app.controller('PeopleCtrl', function($scope, PeopleService, $rootScope) {
+    $rootScope.$on('upwards', function(event,b) {
+        console.log('up', event, b);
+
+    });
+    $scope.$on('downwards', function(event,b) {
+        console.log('down', event , b);
+
+    });
+
+
     $scope.load = function() {
-        $http.get('/api/people').then(
-            function(resp) {
-                $scope.people = resp.data;
+
+        PeopleService.load(function(data) {
+                $scope.people = data;
+
             }
         );
     };
@@ -45,17 +59,51 @@ app.controller('PeopleCtrl', function($scope, $http) {
 //Services
 app.value('AppVersion','1.0');
 
-app.service('PeopleService', function($http) {
-    this.load = function() {
-            $http.post('/api/add',$scope.person)
-                .success(function() {
-                    console.log('Data saved', $location.url());
-                    $location.url('/people');
-                });
+app.service('PeopleService', function($http, $location, $rootScope) {
+    console.log('Create');
 
+    this.save = function(person) {
+        $http.post('/api/add',person)
+            .success(function() {
+                $location.url('/people');
+            });
     };
 
-    this.save = function() {
 
+    this.load = function(callback) {
+
+        $http.get('/api/people').then(
+            function(resp) {
+                $rootScope.$emit('upwards',123);
+                $rootScope.$broadcast('downwards',123);
+
+                callback(resp.data);
+            }
+        );
+
+    };
+});
+
+app.directive('cMenu', function($location, $rootScope) {
+    return {
+        templateUrl: '/javascripts/ng/directives/cmenu.html',
+        restrict: 'EA',
+        scope: {
+            menuText: '@text',
+            location: "@"
+        },
+        link: function(scope, elem, attr) {
+            console.log("Link: ",$location.url());
+            if (scope.location.replace('#','')===$location.url()) {
+                elem.addClass('current');
+            } else {
+                elem.removeClass('current');
+            }
+            //if (scope.location === location.url())
+        }
+        /*compile: function(elem, attr) {
+            console.log("Compile: ",$location.url());
+            //if (scope.location === location.url())
+        }*/
     };
 });
